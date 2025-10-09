0f2f924a210f9534884c62315a534e1fd214b4a1
HervinCajas2 <hervindc15@gmail.com>
2025-10-09 17:16:56 -0500

    Cambios en home, y crear componentes login y register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
 
-/**
- * Descripción: Componente que muestra inicio de secion y registro de usuario
- * Autor: Kevin Acosta.
- * Fecha de creación: 02/10/2025
- */
-
 @Component({
   selector: 'app-register',
-  standalone: true,
+  imports: [],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
