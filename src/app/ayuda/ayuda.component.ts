437e5e2d998315a9195721f1186ea85f067856ac
Vanessa <vanessa@gmail.com>
2025-10-10 15:33:46 -0500

    Se agrego la parte de Ayuda, y se quito logo y fondo

--- src/app/ayuda/ayuda.component.ts
+++ src/app/ayuda/ayuda.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ayuda',
-  imports: [],
   templateUrl: './ayuda.component.html',
-  styleUrl: './ayuda.component.css'
+  styleUrls: ['./ayuda.component.css']
 })
-export class AyudaComponent {
-
-}
+export class AyudaComponent {}
