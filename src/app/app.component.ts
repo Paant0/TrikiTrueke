9986aa423f25a7a39d04aed673fcef3124be40e7
Johanex <kevinjoha342@gmail.com>
2025-10-09 21:00:09 -0500

    ya pueden ver sus html jojojo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+
 import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [ RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `<router-outlet></router-outlet>`
 })
 export class AppComponent {
   title = 'TrikiTrueke';
