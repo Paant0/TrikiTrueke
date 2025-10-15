9c86aa56eea46875513d27f1774a35c4d02bbbbe
Johanex <kevinjoha342@gmail.com>
2025-10-15 11:07:23 -0500

    me falta pero ya casi acabo XD

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
 standalone: true,
   templateUrl: './login.component.html',
+  imports: [RouterLink],
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
