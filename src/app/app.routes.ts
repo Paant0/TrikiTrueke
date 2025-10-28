71da80a95462c47a1de2d34592a5568dddcd5233
Johanex <kevinjoha342@gmail.com>
2025-10-28 10:54:41 -0500

    Cambios en Home, mensajes, y index

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,17 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { CategoriasComponent } from './categorias/categorias.component';
 import { ArticulosComponent } from './articulos/articulos.component';
 import { MensajesComponent } from './mensajes/mensajes.component';
 import { AyudaComponent } from './ayuda/ayuda.component';
 
 export const routes: Routes = [
-   { path: '', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
   { path: 'categorias', component: CategoriasComponent },
   { path: 'articulos', component: ArticulosComponent },
   { path: 'mensajes', component: MensajesComponent },
   { path: 'ayuda', component: AyudaComponent },
-  { path: '**', redirectTo: '' }
 ];
