6d7cb1321e3a1c63c7806bbc652539fff7602a1d
HervinCajas <hervindc15@gmail.com>
2025-11-25 21:17:52 -0500

    Inicio en lo principal de lo principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { MensajesComponent } from './mensajes/mensajes.component';
 import { AyudaComponent } from './ayuda/ayuda.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'categorias', component: CategoriasComponent },
