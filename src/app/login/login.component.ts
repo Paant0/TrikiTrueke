f08973604a73d4e88ee7139113a797ef0d260ed1
Johanex <kevinjoha342@gmail.com>
2026-04-29 11:01:06 -0500

    conexion frontend con backend LOGIN

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,39 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../Services/auth.service';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
 
-  
+  // DATOS LOGIN
+  loginData = {
+    email: '',
+    clave: ''
+  };
+
+  // DATOS REGISTRO
+  registerData = {
+    nombre: '',
+    email: '',
+    telefono: '',
+    clave: ''
+  };
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
   toggleForm(mode: string) {
     const container = document.getElementById('container');
+
     if (mode === 'register') {
       container?.classList.add('active');
     } else {
@@ -19,18 +41,37 @@ export class LoginComponent {
     }
   }
 
-
   onRegister(event: Event) {
     event.preventDefault();
 
-    alert('Cuenta creada exitosamente.\nYa puedes iniciar sesión.');
-    this.toggleForm('login'); 
+    this.authService.register(this.registerData).subscribe({
+      next: (response) => {
+        alert('Usuario registrado correctamente');
+        console.log(response);
+
+        this.toggleForm('login');
+      },
+      error: (error) => {
+        console.log(error);
+        alert('Error al registrar usuario');
+      }
+    });
   }
 
   onLogin(event: Event) {
     event.preventDefault();
 
-    alert(' Inicio de sesión exitoso.');
-    this.router.navigate(['/home']); 
+    this.authService.login(this.loginData).subscribe({
+      next: (response) => {
+        alert('Login exitoso');
+        console.log(response);
+
+        this.router.navigate(['/home']);
+      },
+      error: (error) => {
+        console.log(error);
+        alert('Credenciales incorrectas');
+      }
+    });
   }
 }
